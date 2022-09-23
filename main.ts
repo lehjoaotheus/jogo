78d6558064c49c1f4187bcb2b57ca7cbe8b17840
lehjoaotheus <114233920+lehjoaotheus@users.noreply.github.com>
2022-09-23 09:40:57 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+let p2 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("let´s play girl")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    p2 += 1
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    p2 += 1
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
